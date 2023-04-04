0efd6054b78f9e9009fa2c3ce106f9b000991b6a
Helton-Pereira <helton.bfsm@gmail.com>
2023-04-04 19:31:02 -0300

    Req 5 porém com erro de linter (too complex)

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,16 +1,24 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @staticmethod
     def import_data(path, type):
         list_dict = []
-        with open(path, encoding='utf-8') as file:
-            file_reader = csv.DictReader(file, delimiter=',')
-            for product in file_reader:
-                list_dict.append(product)
+        if path[(len(path) - 1)] == 'v':
+            with open(path, encoding='utf-8') as file:
+                file_reader = csv.DictReader(file, delimiter=',')
+                for product in file_reader:
+                    list_dict.append(product)
+
+        elif path[(len(path) - 1)] == 'n':
+            with open(path, encoding='utf-8') as file:
+                file_reader = json.load(file)
+                for product in file_reader:
+                    list_dict.append(product)
 
         if type == 'simples':
             return SimpleReport.generate(list_dict)
