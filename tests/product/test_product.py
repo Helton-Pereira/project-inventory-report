6f8ab13bdede4d34195823ca4de6c43214bd6e27
Helton-Pereira <helton.bfsm@gmail.com>
2023-04-04 15:22:35 -0300

    Req 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    ...  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Produto X',
+        'Empresa Y',
+        '25/12/2022',
+        'Sem expiração',
+        '123456',
+        'em local seco')
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'Produto X'
+    assert product.nome_da_empresa == 'Empresa Y'
+    assert product.data_de_fabricacao == '25/12/2022'
+    assert product.data_de_validade == 'Sem expiração'
+    assert product.numero_de_serie == '123456'
+    assert product.instrucoes_de_armazenamento == 'em local seco'
