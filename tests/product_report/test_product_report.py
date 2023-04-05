aa8e2814e893051f2b46c9ac4dbfb0026461dbd0
Helton-Pereira <helton.bfsm@gmail.com>
2023-04-05 16:34:43 -0300

    Req 08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Produto X',
+        'Empresa Y',
+        '25/12/2022',
+        'Sem expiração',
+        '123456',
+        'em local seco')
+
+    assert product.__repr__() == (
+        'O produto Produto X fabricado em 25/12/2022 '
+        'por Empresa Y com validade até Sem expiração '
+        'precisa ser armazenado em local seco.')
